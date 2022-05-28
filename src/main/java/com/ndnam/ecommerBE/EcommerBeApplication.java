fc338e0e8dd799cfd78cb9020ed163870b387650
namnd0312 <namnd0312@gmail.com>
2022-05-28 21:27:18 +0700

    init project

---
+++ src/main/java/com/ndnam/ecommerBE/EcommerBeApplication.java
@@ -0,0 +1,13 @@
+package com.ndnam.ecommerBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ndnam/ecommerBE/EcommerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ndnam.ecommerBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
